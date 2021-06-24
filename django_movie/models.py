e531efec9affc38faa3dde7f3bf7bb02f2c2f6a2
Xarakiri <rafikov.ds777@gmail.com>
2021-06-24 23:43:10 +0500

    add genre model

--- django_movie/models.py
+++ django_movie/models.py
@@ -28,3 +28,17 @@ class Actor(models.Model):
     class Meta:
         verbose_name = 'Актеры и режжисеры'
         verbose_name_plural = 'Актеры и режжисеры'
+
+
+class Genre(models.Model):
+    """Жанры"""
+    name = models.CharField('Имя', max_length=100)
+    description = models.TextField('Описание')
+    url = models.SlugField(max_length=160, unique=True)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = 'Жанр'
+        verbose_name_plural = 'Жанр'
