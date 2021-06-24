6a1ed9534d10a8a416fe5bf714e9c86fbfd3af22
Xarakiri <rafikov.ds777@gmail.com>
2021-06-24 23:40:38 +0500

    add actor model

--- django_movie/models.py
+++ django_movie/models.py
@@ -13,3 +13,18 @@ class Category(models.Model):
     class Meta:
         verbose_name = 'Категория'
         verbose_name_plural = 'Категория'
+
+
+class Actor(models.Model):
+    """Актеры и режжисеры"""
+    name = models.CharField('Имя', max_length=100)
+    age = models.PositiveSmallIntegerField('Возраст', default=0)
+    description = models.TextField('Описание')
+    image = models.ImageField('Изображение', upload_to='actors/')
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = 'Актеры и режжисеры'
+        verbose_name_plural = 'Актеры и режжисеры'
