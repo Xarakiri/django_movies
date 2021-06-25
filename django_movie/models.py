b28b92fb2a787bd7e541e4c69776a3e15c15de3b
Xarakiri <rafikov.ds777@gmail.com>
2021-06-25 21:09:34 +0500

    add movie model

--- django_movie/models.py
+++ django_movie/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import date
 
 
 class Category(models.Model):
@@ -42,3 +43,37 @@ class Genre(models.Model):
     class Meta:
         verbose_name = 'Жанр'
         verbose_name_plural = 'Жанр'
+
+
+class Movie(models.Model):
+    """Фильм"""
+    title = models.CharField('Название', max_length=100)
+    tagline = models.CharField('Слоган', max_length=100, default='')
+    description = models.TextField('Описание')
+    poster = models.ImageField('Постер', upload_to='movies/')
+    year = models.PositiveSmallIntegerField('Дата выхода', default=2021)
+    country = models.CharField('Страна', max_length=30)
+    directors = models.ManyToManyField(Actor, verbose_name='режжисер', related_name='film_director')
+    actors = models.ManyToManyField(Actor, verbose_name='актеры', related_name='film_actor')
+    genres = models.ManyToManyField(Genre, verbose_name='жанры')
+    world_premiere = models.DateField('Премьера в мире', default=date.today)
+    budget = models.PositiveIntegerField('Бюджет', default=0, help_text='указывать сумму в долларах')
+    fess_in_usa = models.PositiveIntegerField(
+        'Сборы в США', default=0, help_text='указывать сумму в долларах'
+    )
+    fess_in_world = models.PositiveIntegerField(
+        'Сборы в мире', default=0, help_text='указывать сумму в долларах'
+    )
+    category = models.ForeignKey(
+        Category, verbose_name='Категория', on_delete=models.SET_NULL, null=True
+    )
+    url = models.SlugField(max_length=130, unique=True)
+    draft = models.BooleanField('Черновик', default=False)
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        verbose_name = 'Фильм'
+        verbose_name_plural = 'Фильм'
+
