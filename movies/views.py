2ca08da59cb2648e8e640283583fde7f5a3affe5
Xarakiri <rafikov.ds777@gmail.com>
2021-07-22 22:50:51 +0500

    use DetailView

--- movies/views.py
+++ movies/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.base import View
 from django.views.generic import ListView
+from django.views.generic import DetailView
 from .models import Movie
 
 
@@ -8,12 +9,10 @@ class MoviesView(ListView):
     """Список фильмов"""
     model = Movie
     queryset = Movie.objects.filter(draft=False)
-    template_name = 'movies/movies.html'
 
 
-class MovieDetailView(View):
+class MovieDetailView(DetailView):
     """Полное описание фильма"""
-    def get(self, request, slug):
-        movie = Movie.objects.get(url=slug)
-        return render(request, 'movies/movie_detail.html', {'movie': movie})
+    model = Movie
+    slug_field = 'url'
 
